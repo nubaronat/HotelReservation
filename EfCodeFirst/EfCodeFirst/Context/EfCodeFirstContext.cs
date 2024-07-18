6501168a1a3e4aae3d7680d7105ae4ccf1595c60
exmozturk <exmozturk@gmail.com>
2024-07-18 16:37:47 +0300

    First Commit

---
+++ EfCodeFirst/EfCodeFirst/Context/EfCodeFirstContext.cs
@@ -0,0 +1,21 @@
+﻿using EfCodeFirst.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCodeFirst.Context
+{
+    public class EfCodeFirstContext:DbContext
+    {
+        public EfCodeFirstContext() : base("Server=localhost;Database=EfCodeFirst;User Id=sa;Password=Paycoop123!")
+        {
+
+        }
+        public DbSet<Product>Products { get; set; }
+        public DbSet<Category>Categories { get; set; }
+
+    }
+}
---
+++ EfCodeFirst/EfCodeFirst/Migrations/202407181159191_VeriTabaniOlustur.cs
@@ -0,0 +1,37 @@
+﻿namespace EfCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class VeriTabaniOlustur : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CategoryName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProductName = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
